ac45e6688401b658dfe2718e27b9685774a059d3
Bobby Dilley <bobbydilley@live.com>
2017-09-25 17:08:38 +0100

    Add comments

--- Monitor.php
+++ Monitor.php
@@ -1,7 +1,10 @@
 <?php
 
 echo "Monitoring program started\n";
+// Wait time changes how many seconds should be waited before running another set of tests
 $wait_time = 10;
+
+
 $running = true;
 while($running) {
 	sleep($wait_time);
@@ -9,10 +12,16 @@ while($running) {
 	
 	# Monitor PI
 	$time = date("h:i:sa");
+
+	// Check status is used to run a script
 	checkStatus("./input/check_pi_temp", "Raspberry Pi Temperature");
 	checkStatus("./input/check_website_up", "Dilley.io website available");
 }
 
+/**
+ * $path - The path to the script that will be run
+ * $name - The name to give the script for messaging
+ */
 function checkStatus($path, $name) {
 	$time = date("h:i:sa");
 	$output = exec($path);
@@ -26,6 +35,10 @@ function checkStatus($path, $name) {
 	}
 }
 
+/**
+ * $path - The path to the output script that will be run
+ * $message - The message to alert with
+ */
 function runOutput($path, $message) {
 	exec($path . " " . $message);
 }
